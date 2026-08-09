606cac66c08e2160547e90678e5bb9a6b6ca2aa7
witosis-gpt <witosis@gmail.com>
2026-08-09 08:41:14 +0700

    Load integrated Gantt feature in main tracker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'carnivore-daily-v3';
-const ASSETS = ['./', './index.html', './output.html', './manifest.webmanifest'];
+const CACHE = 'carnivore-daily-v4';
+const ASSETS = ['./', './index.html', './gantt.js', './manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))));
